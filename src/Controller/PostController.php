726bb87999fe0fd3eaa6db9b9a896ad083f422e5
Kévin <jovene.kevin@gmail.com>
2025-04-03 15:04:37 +0200

    :white_check_mark: Importation des données depuis la base SQL

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PostController extends AbstractController
 {
     #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    public function show(PostRepository $postRepository, UserRepository $userRepository): Response
     {
+
+        $posts = $postRepository -> findAll();
+        $users = $userRepository -> findAll();
+        $user = $this->getUser();
+
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts,
+            'users' => $users
         ]);
     }
 }
